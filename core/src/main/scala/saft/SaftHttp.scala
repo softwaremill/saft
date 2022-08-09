2cb15003a3bcb40ec9311b1b4f8cd167034b6505
adamw <adam@warski.org>
2022-08-09 18:32:25 +0200

    WIP

--- core/src/main/scala/saft/SaftHttp.scala
+++ core/src/main/scala/saft/SaftHttp.scala
@@ -1,5 +1,6 @@
 package saft
 
+import sttp.client3.SttpBackend
 import sttp.client3.httpclient.zio.HttpClientZioBackend
 import zio.*
 import zio.json.*
@@ -11,10 +12,7 @@ object SaftHttp2 extends SaftHttp(2)
 object SaftHttp3 extends SaftHttp(3)
 
 /** A Raft implementation using json-over-http for inter-node communication. */
-class SaftHttp(nodeNumber: Int) extends JsonCodecs with ZIOAppDefault with Logging {
-  private case class TimedOutException(msg: String) extends Exception(msg)
-  private case class DecodeException(msg: String) extends Exception(msg)
-
+class SaftHttp(nodeNumber: Int) extends ZIOAppDefault with JsonCodecs with Logging {
   override val run: Task[Nothing] =
     // configuration
     val conf = Conf.default(3)
@@ -23,49 +21,27 @@ class SaftHttp(nodeNumber: Int) extends JsonCodecs with ZIOAppDefault with Loggi
 
     // setup node
     val nodeId = NodeId(nodeNumber)
+    def nodePort(nodeId: NodeId): Int = 8080 + nodeId.number
+    val port = nodePort(nodeId)
 
     for {
       stateMachine <- StateMachine.background(applyLogData(nodeId))
       persistence <- InMemoryPersistence(List(nodeId)).map(_.forNodeId(nodeId))
       queue <- Queue.sliding[ServerEvent](16)
       backend <- HttpClientZioBackend()
-      comms = new Comms {
-        override def nextEvent: UIO[ServerEvent] = queue.take
-        override def send(toNodeId: NodeId, msg: RequestMessage with FromServerMessage): UIO[Unit] =
-          import sttp.client3.*
-          val url = uri"http://localhost:${nodePort(toNodeId)}/${endpoint(msg)}"
-          backend
-            .send(basicRequest.post(url).body(encodeRequest(msg)).response(asStringAlways))
-            .timeoutFail(TimedOutException(s"Client request $msg to $url"))(clientTimeout)
-            .map(_.body)
-            .flatMap { body =>
-              decodeResponse(msg, body) match
-                case Left(errorMsg) => ZIO.fail(DecodeException(s"Client request $msg to $url, response: $body, error: $errorMsg"))
-                case Right(decoded) => queue.offer(ResponseReceived(decoded))
-            }
-            .unit
-            .catchAll { case e: Exception => ZIO.logErrorCause(s"Cannot send $msg to $toNodeId", Cause.fail(e)) }
-        override def add(event: ServerEvent): UIO[Unit] = queue.offer(event).unit
-      }
+      comms = new HttpComms(queue, backend, clientTimeout, nodePort)
       node = new Node(nodeId, comms, stateMachine, conf, persistence)
-      port = nodePort(nodeId)
       _ <- ZIO.log(s"Starting SaftHttp on localhost:$port")
       _ <- ZIO.log(s"Configuration: ${conf.show}")
       _ <- node.start.fork
       result <- Server.start(port, app(queue))
     } yield result
 
-  private def nodePort(nodeId: NodeId): Int = 8080 + nodeId.number
-
-  val AppendEntriesEndpoint = "append-entries"
-  val RequestVoteEndpoint = "request-vote"
-  val NewEntryEndpoint = "new-entry"
-
   private def app(queue: Queue[ServerEvent]): HttpApp[Any, Throwable] = Http
     .collectZIO[Request] {
-      case r @ Method.POST -> !! / AppendEntriesEndpoint => decodingEndpoint(r, _.fromJson[AppendEntries], queue)
-      case r @ Method.POST -> !! / RequestVoteEndpoint   => decodingEndpoint(r, _.fromJson[RequestVote], queue)
-      case r @ Method.POST -> !! / NewEntryEndpoint      => decodingEndpoint(r, _.fromJson[NewEntry], queue)
+      case r @ Method.POST -> !! / EndpointNames.AppendEntries => decodingEndpoint(r, _.fromJson[AppendEntries], queue)
+      case r @ Method.POST -> !! / EndpointNames.RequestVote   => decodingEndpoint(r, _.fromJson[RequestVote], queue)
+      case r @ Method.POST -> !! / EndpointNames.NewEntry      => decodingEndpoint(r, _.fromJson[NewEntry], queue)
     }
     .catchAll {
       case e: TimedOutException => Http.fromZIO(ZIO.logErrorCause(Cause.fail(e)).as(Response(Status.RequestTimeout)))
@@ -87,11 +63,35 @@ class SaftHttp(nodeNumber: Int) extends JsonCodecs with ZIOAppDefault with Loggi
         } yield Response.text(encodeResponse(r))
       case Left(errorMsg) => ZIO.fail(DecodeException(s"Handling request message: $errorMsg"))
     }
+}
+
+private case class TimedOutException(msg: String) extends Exception(msg)
+private case class DecodeException(msg: String) extends Exception(msg)
+
+private class HttpComms(queue: Queue[ServerEvent], backend: SttpBackend[Task, Any], clientTimeout: Duration, nodePort: NodeId => Int)
+    extends Comms
+    with JsonCodecs {
+  override def nextEvent: UIO[ServerEvent] = queue.take
+  override def send(toNodeId: NodeId, msg: RequestMessage with FromServerMessage): UIO[Unit] =
+    import sttp.client3.*
+    val url = uri"http://localhost:${nodePort(toNodeId)}/${endpoint(msg)}"
+    backend
+      .send(basicRequest.post(url).body(encodeRequest(msg)).response(asStringAlways))
+      .timeoutFail(TimedOutException(s"Client request $msg to $url"))(clientTimeout)
+      .map(_.body)
+      .flatMap { body =>
+        decodeResponse(msg, body) match
+          case Left(errorMsg) => ZIO.fail(DecodeException(s"Client request $msg to $url, response: $body, error: $errorMsg"))
+          case Right(decoded) => queue.offer(ResponseReceived(decoded))
+      }
+      .unit
+      .catchAll { case e: Exception => ZIO.logErrorCause(s"Cannot send $msg to $toNodeId", Cause.fail(e)) }
+  override def add(event: ServerEvent): UIO[Unit] = queue.offer(event).unit
 
   private def endpoint(msg: RequestMessage): String = msg match
-    case _: AppendEntries => AppendEntriesEndpoint
-    case _: RequestVote   => RequestVoteEndpoint
-    case _: NewEntry      => NewEntryEndpoint
+    case _: AppendEntries => EndpointNames.AppendEntries
+    case _: RequestVote   => EndpointNames.RequestVote
+    case _: NewEntry      => EndpointNames.NewEntry
 }
 
 private trait JsonCodecs {
@@ -137,4 +137,10 @@ private trait JsonCodecs {
     toRequest match
       case _: RequestVote   => data.fromJson[RequestVoteResponse]
       case _: AppendEntries => data.fromJson[AppendEntriesResponse]
+}
+
+object EndpointNames {
+  val AppendEntries = "append-entries"
+  val RequestVote = "request-vote"
+  val NewEntry = "new-entry"
 }
